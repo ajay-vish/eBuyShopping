2538da4fe33ef1f876dc97d0c1403662a131ef29
HarshKunte <harshkunte99@gmail.com>
2021-07-20 00:51:52 +0530

    product-template styles updated

--- client/src/app/components/products/product-template/product-template.component.ts
+++ client/src/app/components/products/product-template/product-template.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
+import {
+  MatSnackBar,
+} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-product-template',
   templateUrl: './product-template.component.html',
@@ -11,11 +14,59 @@ export class ProductTemplateComponent implements OnInit {
   photo = 'assets/images/loading.gif';
   jwt: any;
   cart: any;
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.productService.getProduct(this.product._id).subscribe((resp: any) => {
       this.photo = resp.photo.contentType;
+      this.product = resp;
     })
   }
+
+
+  // if add to cart functionality is to be used
+  //uncomment below code
+
+  // addItemToCart(item: { _id: any }, event:any) {
+  //   event.stopPropagation()
+  //   let cart = [];
+  //   if (typeof window !== undefined) {
+  //     if (localStorage.getItem('cart')) {
+  //       cart = JSON.parse(localStorage.getItem('cart') || '');
+  //     }
+  //     if (!this.checkAlreadyInCart(cart, item)) {
+       
+  //       cart.push({
+  //         ...item,
+  //         count: 1,
+  //       });
+  //       this.snackBar.open('Item added to cart!', 'close', {
+  //         duration: 2000,
+  //         panelClass: ['success-snackbar'],
+  //         horizontalPosition: 'center',
+  //         verticalPosition: 'top',
+  //       });
+  //     }
+  //     else{
+  //       this.snackBar.open('Item already in cart!', 'close', {
+  //         duration: 2000,
+  //         panelClass: ['warning-snackbar'],
+  //         horizontalPosition: 'center',
+  //         verticalPosition: 'top',
+  //       });
+  //     }
+  //     localStorage.setItem('cart', JSON.stringify(cart));
+  //   }
+  // }
+
+  // checkAlreadyInCart(cart: any[], item: { _id: any }) {
+  //   if (
+  //     cart.filter((product: { _id: any }) => {
+  //       return product._id === item._id;
+  //     }).length > 0
+  //   ) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 }
